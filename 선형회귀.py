8999ab7285fb78c82ec4193243e182ca255a5f14
전또맹 <eunjiamy@naver.com>
2020-11-04 13:27:13 +0900

    Create 선형회귀.py
    init

---
+++ 선형회귀.py
@@ -0,0 +1,21 @@
+import numpy
+
+def main():
+    x = [1,2,3,4,5,6,7,8]
+    y = [3,6,5,9,10,14,18,20]
+    N = 8
+
+    X = numpy.array(x)
+    Y = numpy.array(Y)
+
+    results = do_linear_regression(X,Y,N)
+
+def do_linear_regression(X,Y,N):
+    
+
+def cost(X,Y,N,a,b):
+    cost = 0
+    for i in
+
+if __name__ == "__main__":
+    main()
