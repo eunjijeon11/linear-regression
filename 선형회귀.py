eeaca10b9c1310e0676e5f1d1a7dbb508b86eb96
전또맹 <eunjiamy@naver.com>
2020-11-04 21:22:56 +0900

    Update 선형회귀.py
    완성

--- 선형회귀.py
+++ 선형회귀.py
@@ -1,21 +1,34 @@
-import numpy
+from matplotlib.pylab import plot, show, scatter
 
 def main():
-    x = [1,2,3,4,5,6,7,8]
-    y = [3,6,5,9,10,14,18,20]
+    X = [1,2,3,4,5,6,7,8]
+    Y = [3,6,5,9,10,14,18,20]
     N = 8
-
-    X = numpy.array(x)
-    Y = numpy.array(Y)
-
-    results = do_linear_regression(X,Y,N)
-
-def do_linear_regression(X,Y,N):
     
+    w, b = do_linear_regression(X, Y, N)
+    print(w,b)
+    y = [(w * number + b) for number in X]
+    scatter(X,Y)
+    plot(X,y)
+    show()
 
-def cost(X,Y,N,a,b):
-    cost = 0
-    for i in
+def do_linear_regression(X, Y, N, rate=0.001, epochs=1000):
+    w_0 = 1.0
+    b_0 = 1.0
+    for t in range(epochs):
+        w_grad, b_grad = gradient_descendant(X,Y,N,w_0,b_0)
+        w_0 -= rate * w_grad
+        b_0 -= rate * b_grad
+    return w_0, b_0
+
+def gradient_descendant(X,Y,N,w_0,b_0):
+    w_grad = 0
+    b_grad = 0
+    for i in range(N):
+        w_grad += X[i] * (X[i] * w_0 + b_0 -Y[i])
+        b_grad += (X[i] * w_0 + b_0 - Y[i])
+    return w_grad, b_grad
+    
 
 if __name__ == "__main__":
     main()
